e3a17a547837772fe4b572e7997d0d5d81d034ca
Emil Abdullaev <renoemilll@gmail.com>
2025-06-01 03:16:00 +0600

    schemas.py comments added + minor changes

--- app/database.py
+++ app/database.py
@@ -4,14 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-#задаём строку подключения к нашему файлу-базе tasks.db
+# задаём строку подключения к нашему файлу-базе tasks.db
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./tasks.db'
 
-#создаём движок SQLAlchemy, который будет посредником между кодом и бд
+# создаём движок SQLAlchemy, который будет посредником между кодом и бд
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False},      #нужно для корректной работы бд в многопоточном режиме
-    echo=True)      #выводит в консоли все SQL запросы
+    connect_args={'check_same_thread': False},      # нужно для корректной работы бд в многопоточном режиме
+    echo=True)      # выводит в консоли все SQL запросы
 
-#фабрика сессий для выполнения операций crud
+# фабрика сессий для выполнения операций crud
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/schemas.py
+++ app/schemas.py
@@ -3,22 +3,27 @@ from typing import Optional, List
 from pydantic import BaseModel, Field
 from .models import StatusEnum
 
+# создаём базу объектам валидации и преобразования
 class TaskBase(BaseModel):
     title: str = Field(..., min_Length=1)
     description: Optional[str] = None
     due_date: Optional[datetime] = None
     status: Optional[StatusEnum] = StatusEnum.new
 
+# сама задача наследуется от базы
 class TaskCreate(TaskBase):
     pass
 
+# схема для обновления задачи, где все поля необязательны к заполнению
+# так же пустые поля обрезаются
 class TaskUpdate(BaseModel):
     title: Optional[str]
     description: Optional[str]
     due_date: Optional[datetime]
     status: Optional[StatusEnum]
 
+# схема для ответа пользователю
 class TaskOut(TaskBase):
     id: int
     class Config:
-        orm_mode = True
+        orm_mode = True     #разрешаем читать напрямую из бд

bd6f5f7723ff744bad11fb82406f9dfe8a633640
Emil Abdullaev <renoemilll@gmail.com>
2025-06-01 02:55:50 +0600

    database.py comments added + echo added

--- app/database.py
+++ app/database.py
@@ -4,6 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+#задаём строку подключения к нашему файлу-базе tasks.db
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./tasks.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+#создаём движок SQLAlchemy, который будет посредником между кодом и бд
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={'check_same_thread': False},      #нужно для корректной работы бд в многопоточном режиме
+    echo=True)      #выводит в консоли все SQL запросы
+
+#фабрика сессий для выполнения операций crud
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

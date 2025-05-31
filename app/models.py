906ac7257732a2ca8dff916391a0ecbc35a7bf76
Emil Abdullaev <renoemilll@gmail.com>
2025-06-01 03:00:07 +0600

    models.py comments added

--- app/models.py
+++ app/models.py
@@ -2,15 +2,23 @@ from sqlalchemy import Column, Integer, String, DateTime, Enum
 import enum
 from .database import Base
 
+#класс для определения статуса задачи
 class StatusEnum(str, enum.Enum):
     new = "new"
     in_progress = "in_progress"
     done = "done"
 
+#класс самой задачи
 class Task(Base):
+
     __tablename__ = 'tasks'
+
     id = Column(Integer, primary_key=True, index=True)
+
     title = Column(String, index=True, nullable=False)
+
     description = Column(String, nullable=False)
+
     due_date = Column(DateTime, nullable=False)
+
     status = Column(Enum(StatusEnum), default=StatusEnum.new, nullable=False)

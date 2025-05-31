bb12defaf19378e390dd58dbf36fe8330ad4f51d
Emil Abdullaev <renoemilll@gmail.com>
2025-06-01 03:25:00 +0600

    auth.py comments added

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,11 @@
 from fastapi import Header, HTTPException, status
 
+# "захардкоденный секретный токен"
 API_TOKEN = "hardcoded-secret-token"
 
+# проверяем токен и либо проходим дальше, либо возвращаем код ошибки с комментом
 def get_token_header(x_token: str = Header(...)):
     if x_token != API_TOKEN:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token")

e174005c24a021aa766e233be88188e31f420f81
Emil Abdullaev <renoemilll@gmail.com>
2025-06-01 03:19:36 +0600

    crud.py comments added

--- app/crud.py
+++ app/crud.py
@@ -2,9 +2,11 @@ from pyexpat import model
 from sqlalchemy.orm import Session
 from . import models, schemas
 
+# получить задачу по ID
 def get_task(db: Session, task_id: int):
     return db.query(models.Task).filter(models.Task.id == task_id).first()
 
+# получить список задач отфильтрованных либо по статусу, либо по дате
 def get_tasks(db: Session, status=None, due_date=None):
     q = db.query(model.Task)
     if status:
@@ -13,6 +15,7 @@ def get_tasks(db: Session, status=None, due_date=None):
         q = q.filter(model.Task.due_date == due_date)
     return q.all()
 
+# создать новую задачу
 def create_task(db: Session, task: schemas.TaskCreate):
     db_task = models.Task(**task.model.dump())
     db.add(db_task)
@@ -20,6 +23,7 @@ def create_task(db: Session, task: schemas.TaskCreate):
     db.refresh(db_task)
     return db_task
 
+# обновляем задачу
 def update_task(db: Session, db_task: models.Task, updates: schemas.TaskUpdate):
     for field, value in updates.model_dump(exclude_unset=True).items():
         setattr(db_task, field, value)
@@ -27,6 +31,7 @@ def update_task(db: Session, db_task: models.Task, updates: schemas.TaskUpdate):
     db.refresh(db_task)
     return db_task
 
+# удаляем задачу
 def delete_task(db: Session, db_task: models.Task):
     db.delete(db_task)
     db.commit()
